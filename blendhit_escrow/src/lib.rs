524d31b57ee43b6df84ee0b534e07b725a3071c0
azuldev <91290695+redazul@users.noreply.github.com>
2023-02-20 21:03:48 -0700

    Update lib.rs

--- blendhit_escrow/src/lib.rs
+++ blendhit_escrow/src/lib.rs
@@ -2,23 +2,37 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
-    pubkey::Pubkey,
     msg,
-    //program_error::ProgramError,
+    program_error::ProgramError,
+    pubkey::Pubkey,
 };
 
 entrypoint!(process_instruction);
 
-fn process_instruction(_program_id: &Pubkey, accounts: &[AccountInfo], _instruction_data: &[u8]) -> ProgramResult {
-    // Ensure that there is only one account being passed to the program
+// Program entrypoint
+fn process_instruction(
+    program_id: &Pubkey, // Public key of the program account
+    accounts: &[AccountInfo], // Accounts passed to the program
+    _instruction_data: &[u8], // Instruction data passed to the program
+) -> ProgramResult {
+
+    // Iterate through the accounts passed to the program
     let accounts_iter = &mut accounts.iter();
-    let caller_info = next_account_info(accounts_iter)?;
+    let user_account_info = next_account_info(accounts_iter)?;
 
-    // Get the public key of the caller
-    let caller_key = caller_info.key;
+    // Check that the user account is owned by the program
+    if user_account_info.owner != program_id {
+        msg!("User account does not have the correct program ID");
+        return Err(ProgramError::IncorrectProgramId);
+    }
 
-    // Log the caller's public key
-    msg!("Caller's pubkey: {}", caller_key);
+    // Get the public key passed by the user
+    let user_pubkey_info = next_account_info(accounts_iter)?;
+    let user_pubkey = user_pubkey_info.key;
+
+    // Store the user's public key in the user account
+    msg!("Storing user public key: {:?}", user_pubkey);
+    user_account_info.data.borrow_mut()[0..32].copy_from_slice(user_pubkey.as_ref());
 
     Ok(())
 }
